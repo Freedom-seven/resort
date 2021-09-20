611a32f11518f658b442174f3ce090da7cf2d79d
FREEDOM <freewhynane62@gmail.com>
2021-09-20 11:40:46 +0000

    returen room component

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Rooms from './Room'
 
-export default function RoomsList() {
+export default function RoomsList({rooms}) {
+    if(rooms.length === 0)
+ return (
+     <div className='empty-search'>
+         <h3>unfortunaktely no rooms matched your search parameters</h3>
+     </div>
+ )
+
+
     return (
-        <div>
-            hello room list
-        </div>
+        <section className='roomslist'>
+            <div className='roomslist-center'>
+                {
+                    rooms.map(item => {
+                        return <Rooms key={item.id} room={item} />
+                    })
+                }
+            </div>
+        </section>
     )
 }
